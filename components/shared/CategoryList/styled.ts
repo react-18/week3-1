4406f3083e682eeed2b80c0686cd02e1f0455063
rachel490 <ccori12@naver.com>
2022-02-15 15:24:10 +0900

    Feat: category list box 컴포넌트 구현

---
+++ components/shared/CategoryList/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ListContainer = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 112px);
+  gap: 2px;
+`;
+
+export const Wrap = styled.div`
+  padding: 20px;
+`;

fe5e34c3a839de59cb1d0ff3c6fc303dc7aa3ac7
gy <gayunlee11@gmail.com>
2022-02-15 11:12:55 +0900

    Feat: useAxios 추가

---
+++ hooks/useAxios.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const useAxios = (axiosParams: AxiosRequestConfig) => {
+  const [response, setResponse] = useState<AxiosResponse>();
+  const [error, setError] = useState<AxiosError>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = async (params: AxiosRequestConfig) => {
+    try {
+      const result = await axios.request(params);
+      setResponse(result);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(axiosParams);
+  }, []);
+
+  return { response, error, loading };
+};
+
+export default useAxios;
